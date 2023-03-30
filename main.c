29c58cd9c6b7dd974662451c856e32251849a564
rclement-afk <59667786+rclement-afk@users.noreply.github.com>
2023-03-30 10:02:31 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -4,19 +4,20 @@
 #include <consts.h>
 int main(){ 
     int main_speed = 250;
-    int cube_reading=0;
+    //int cube_reading=0;
     create_connect();
     printf("CONNECTED\n");
+    printf("%d\n",get_create_battery_charge());
     set_servo_position(0,350);
     set_servo_position(2,550);
     set_servo_position(3,550);
     enable_servos();    
     slow_servo(0,1800,2);
     square_up_front_create(3,main_speed);
-    create_backward(3,main_speed);
+    //create_backward(3,main_speed);
     msleep(50);
     rotate(-90);
-    create_backward(12,main_speed);
+    create_backward(15,main_speed);
     slow_servo(2,1870,2);
     msleep(50);
     slow_servo(3,1600,2);
@@ -29,7 +30,7 @@ int main(){
     square_up_back_create(1,main_speed);
     slow_servo(2,1870,2);
     slow_servo(3,550,2);
-    slow_servo(2,1500,2);
+    slow_servo(2,700,2);
     //drop pom 1
     create_forward(5,main_speed);
     msleep(50);
@@ -61,7 +62,7 @@ int main(){
     slow_servo(3,550,2);
     slow_servo(2,1650,2);
     //drop pom 3
-    rotate(-33);
+    rotate(-27);
     msleep(50);
     create_forward(15,main_speed);
     msleep(60);
@@ -69,27 +70,28 @@ int main(){
     slow_servo(3,1600,2);
     slow_servo(2,1450,2);
     //grab pom 4
-    create_backward(15,main_speed);
-    rotate(30);
-    square_up_back_create(1,-1*main_speed);
-    create_backward(5,main_speed);
+    rotate(-70);
+    create_backward(10,main_speed);
+    rotate(-90);
     square_up_back_create(1,main_speed);
-    rotate(185);
-    create_backward(30,main_speed);
-    square_up_back_create(1,main_speed);
-    create_backward(5,main_speed);
+    //create_backward(5,main_speed);
+    //square_up_back_create(1,main_speed);
+    //rotate(185);
+    //create_backward(35,main_speed);
+    //square_up_back_create(1,main_speed);
+    //create_backward(5,main_speed);
     slow_servo(3,550,2);
     //drop pom 4
-    rotate(160);
-    create_forward(20,main_speed);
+    rotate(163);
+    create_forward(29,main_speed);
     slow_servo(2,1870,2);
     msleep(50);
     slow_servo(3,1500,2);
-    slow_servo(2,1650,2);
+    slow_servo(2,1050,2);
     //grab pom 5
     create_backward(20,main_speed);
-    rotate(30);
-    create_backward(40,main_speed);
+    rotate(25);
+    create_backward(45,main_speed);
     square_up_back_create(2,main_speed);
     slow_servo(2,1870,2);
     slow_servo(3,550,2);
@@ -100,8 +102,8 @@ int main(){
     create_backward(15,main_speed);
     square_up_back_create(2,main_speed);
     create_forward(8,main_speed);
-    rotate(90);
-    msleep(3000);
+    rotate(93);
+    //msleep(3000);
     slow_servo(0,1855,2);
     slow_servo(2,1750,2);
     slow_servo(3,621,2);
@@ -109,12 +111,12 @@ int main(){
     	create_drive_straight(main_speed);
         msleep(15);
     }
-    create_stop();
+	create_forward(5,main_speed);
     slow_servo(3,1100,2);
     slow_servo(2,900,2);
     create_backward(20,main_speed);
     rotate(90);
-    create_backward(15,main_speed);
+    create_backward(20,main_speed);
     square_up_front_create(3,main_speed);
     create_forward(15,main_speed);
     rotate(89);
@@ -124,65 +126,52 @@ int main(){
         msleep(15);
     }
     create_stop();
-    create_backward(3,main_speed);
+    create_backward(4,main_speed);
     slow_servo(3,621,2);
     slow_servo(2,1615,2);
     slow_servo(3,1100,2);
     slow_servo(2,1600,2);
-    rotate(-5);
+    rotate(-10);
     while(analog(1)<1700){
     	create_spin_CW(50);
         msleep(15);
     }
     create_spin_CCW(50);
-        msleep(15);
-    create_forward(5,main_speed);
+        msleep(20);
+    create_forward(6,main_speed);
     slow_servo(3,621,2);
     create_backward(20,main_speed);
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    //square_up_back_create(2,-1*main_speed);
     rotate(80);
-    slow_servo(2,1100,2);
+    slow_servo(0,1300,2);
+    slow_servo(2,400,2);
     create_backward(10,main_speed);
     square_up_back_create(2,main_speed);
-    create_forward(20,main_speed);
-    rotate(90);
-    square_up_front_create(1,main_speed);
-    create_forward(15,main_speed);
+    create_backward(10,main_speed);
     rotate(-90);
+    square_up_back_create(1,main_speed);
+    msleep(50);
+    //create_forward(5,main_speed);
+    rotate(-280);
+    square_up_back_create(1,main_speed);
+    create_forward(40,main_speed);
+    slow_servo(3,1150,2);
+    slow_servo(2,300,2);
     square_up_back_create(2,-1*main_speed);
-    slow_servo(2,1170,2);
-    slow_servo(0,400,2);
-    while(analog(1)<1560){
-    	create_drive_straight(main_speed);
-        msleep(15);
-    }
+    rotate(125);
+    slow_servo(2,1600,2);
+    slow_servo(3,500,2);
+    slow_servo(2,1000,2);
+    rotate(45);
+    create_backward(15,main_speed);
+    square_up_back_create(2,main_speed);
+    create_forward(35,main_speed);
     rotate(-90);
-    msleep(15);
-    create_forward(5,main_speed);
-    slow_servo(0,1265,2);
-    cube_reading=buffer(0);
-    printf("nothing: %d\n",cube_reading);
-    while(buffer(0)>cube_reading-65){
-    	create_drive_straight(0.5*main_speed);
-        msleep(15);
-        int i=0;
-        if(i%5==0){
-        	printf("reading:%d\n",buffer(0));
-        }
-        i++;
-    }
-    create_stop();
-    slow_servo(2,450,2);
-    
-    slow_servo(3,700,2);
-    create_backward(10,main_speed);
-    rotate(90);
-    slow_servo(2,1100,2);
-    msleep(500);
-    create_forward(10,main_speed);
-    slow_servo(3,1100,2);
-    slow_servo(2,600,2);
-    create_backward(10,main_speed);
+    //create_forward(30,main_speed);
+    //create_backward(30,main_speed);
+    rotate(-90);
+    square_up_back_create(2,main_speed);
     create_disconnect();
     return 0;
 }
